34c7a42640e0a8b369d0f4b5f5561c647c376b45
刘宗源 <lzy@liuzongyuandeMacBook-Air.local>
2019-10-22 22:46:14 +0800

    Initial commit

---
+++ js/alert.js
@@ -0,0 +1,377 @@
+//樱桃
+$(document).ready(function () {
+    $('.cherry').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#cherry').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#cherry').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//木瓜
+$(document).ready(function () {
+    $('.papaya').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#papaya').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#papaya').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//草莓
+$(document).ready(function () {
+    $('.strawberry').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#strawberry').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#strawberry').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//圣女果
+$(document).ready(function () {
+    $('.tomato').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#tomato').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#tomato').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//杨桃
+$(document).ready(function () {
+    $('.Carambola').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Carambola').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Carambola').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//番荔枝
+$(document).ready(function () {
+    $('.Sweetsop').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Sweetsop').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Sweetsop').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//番石榴
+$(document).ready(function () {
+    $('.Guava').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Guava').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Guava').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//青枣
+$(document).ready(function () {
+    $('.datas').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#datas').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#datas').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//枇杷
+$(document).ready(function () {
+    $('.Loquat').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Loquat').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Loquat').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//蛇果
+$(document).ready(function () {
+    $('.apple').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#apple').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#apple').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//柠檬
+$(document).ready(function () {
+    $('.Lemon').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Lemon').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Lemon').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//甘蔗
+$(document).ready(function () {
+    $('.Sugar').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Sugar').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Sugar').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//果桑
+$(document).ready(function () {
+    $('.Mulberry').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Mulberry').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Mulberry').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//荔枝
+$(document).ready(function () {
+    $('.Litchi').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Litchi').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Litchi').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//西瓜
+$(document).ready(function () {
+    $('.Watermelon').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Watermelon').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Watermelon').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//莲雾
+$(document).ready(function () {
+    $('.Wax').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Wax').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Wax').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//李子
+$(document).ready(function () {
+    $('.Plum').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Plum').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Plum').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//香瓜
+$(document).ready(function () {
+    $('.Muskmelon').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Muskmelon').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Muskmelon').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//火龙果
+$(document).ready(function () {
+    $('.Pitaya').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Pitaya').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Pitaya').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//菠萝
+$(document).ready(function () {
+    $('.Pineapple').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Pineapple').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Pineapple').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//菠萝蜜
+$(document).ready(function () {
+    $('.Jackfruit').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Jackfruit').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Jackfruit').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//百香果
+$(document).ready(function () {
+    $('.Passion').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Passion').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Passion').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//龙眼
+$(document).ready(function () {
+    $('.Logans').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Logans').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Logans').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//提子
+$(document).ready(function () {
+    $('.Grapes').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Grapes').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Grapes').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//柿子
+$(document).ready(function () {
+    $('.Persimmon').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Persimmon').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Persimmon').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//芒果
+$(document).ready(function () {
+    $('.mango').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#mango').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#mango').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//石榴
+$(document).ready(function () {
+    $('.Pomegranate').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Pomegranate').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Pomegranate').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//无花果
+$(document).ready(function () {
+    $('.Fig').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#Fig').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#Fig').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
+//柑橘
+$(document).ready(function () {
+    $('.orange').click(function () {
+        var hideobj=document.getElementById("hidebg");
+        hidebg.style.display="block";
+        hidebg.style.height=document.body.clientHeight+"px";
+        $('#orange').fadeIn("slow");
+    })
+    $('.close').click(function () {
+        $('#orange').fadeOut("slow");
+        $('#hidebg').fadeOut("slow");
+    })
+})
